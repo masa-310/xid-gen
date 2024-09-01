fd4c34fae79d753e3817426c89e215994fa5c411
Masashi SATO <gmasa.310@gmail.com>
2024-09-01 22:50:28 +0900

    :D

---
+++ go.mod
@@ -0,0 +1,5 @@
+module xid-gen
+
+go 1.17
+
+require github.com/rs/xid v1.6.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rs/xid"
+)
+
+func main() {
+	generatedXID := xid.New()
+	xidString := generatedXID.String()
+	fmt.Println(xidString)
+}
